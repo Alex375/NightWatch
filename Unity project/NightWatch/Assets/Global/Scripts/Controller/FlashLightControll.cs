693da09e6882af7e464bfb68d482c4dd0bbd25c6
Lucas Pluchon <lucas.pluchon@epita.fr>
2020-04-16 19:21:58 +0200

    Added flashlight asset & mutliplayer flashlight

--- Unity project/NightWatch/Assets/Global/Scripts/Controller/FlashLightControll.cs
+++ Unity project/NightWatch/Assets/Global/Scripts/Controller/FlashLightControll.cs
@@ -1,41 +1,62 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class FlashLightControll : MonoBehaviour
+public class FlashLightControll : MonoBehaviourPun
 {
+    public bool HasTorchLamp = false;
     public bool LightOn = false;
     public Light light;
     public float DecraeseBatteryPerUpdate;
+    public GameObject TorchLamp;
+    
 
-    private void Awake()
+    private bool IsOffline;
+
+    private void Start()
     {
-        SetLightOn(LightOn);
+        IsOffline = PhotonNetwork.OfflineMode;
+        
+        SetTorchLamp(HasTorchLamp);
+        
+        if (!IsOffline)
+            photonView.RPC("SetLightOnMulti",RpcTarget.All,LightOn && HasTorchLamp,photonView.ViewID);
+        else
+            SetLightOn(LightOn && HasTorchLamp);
+        
+        
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (LightOn)
+        if (LightOn && HasTorchLamp)
         {
             PlayerManagerLo.instance.CurrentBatteryLevel -= DecraeseBatteryPerUpdate * Time.deltaTime;
             if (PlayerManagerLo.instance.CurrentBatteryLevel <= 0)
             {
-                SetLightOn(false);
+                if (IsOffline)
+                    SetLightOn(false);
+                else if (light.enabled) 
+                    photonView.RPC("SetLightOnMulti",RpcTarget.All,false,photonView.ViewID);
             }
         }
     }
 
     private void OnGUI()
     {
-        if (Event.current.Equals(Event.KeyboardEvent("F")))
+        if (Event.current.Equals(Event.KeyboardEvent("F")) && HasTorchLamp)
         {
-            SetLightOn(!LightOn);
+            if (IsOffline)
+                SetLightOn(!LightOn);
+            else
+                photonView.RPC("SetLightOnMulti",RpcTarget.All,!LightOn,photonView.ViewID);
         }
         
-        if (Event.current.Equals(Event.KeyboardEvent("R")))
+        if (Event.current.Equals(Event.KeyboardEvent("R")) && HasTorchLamp)
         {
             PlayerManagerLo.instance.ChangeBattery();
         }
@@ -44,6 +65,40 @@ public class FlashLightControll : MonoBehaviour
     public void SetLightOn(bool state)
     {
         if (state && PlayerManagerLo.instance.CurrentBatteryLevel <= 0)
+            return;
+        light.enabled = state;
+        LightOn = state;
+    }
+
+    public void SetTorchLamp(bool state)
+    {
+        if (IsOffline)
+        {
+            TorchLamp.SetActive(state);
+            HasTorchLamp = state;
+        }
+        else
+        {
+            photonView.RPC("SetTorchLampMultiplayer",RpcTarget.All,state,photonView.ViewID);
+        }
+    }
+    
+    //______________________RPC FUNCTION
+    
+    [PunRPC]
+    private void SetTorchLampMultiplayer(bool state, int id)
+    {
+        if (photonView.ViewID == id)
+        {
+            TorchLamp.SetActive(state);
+            HasTorchLamp = state;
+        }
+    }
+
+    [PunRPC]
+    private void SetLightOnMulti(bool state, int id)
+    {
+        if (state && PlayerManagerLo.instance.CurrentBatteryLevel <= 0 && photonView.ViewID != id)
             return;
         light.enabled = state;
         LightOn = state;
