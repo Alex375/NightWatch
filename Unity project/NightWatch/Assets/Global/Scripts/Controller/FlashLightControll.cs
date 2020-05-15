63b814a415a88121c85b894bc5d628f34766aa68
Alex375 <alexandre.josien@gmail.com>
2020-05-15 15:54:21 +0200

    added flicker effect at 25% flashlight

--- Unity project/NightWatch/Assets/Global/Scripts/Controller/FlashLightControll.cs
+++ Unity project/NightWatch/Assets/Global/Scripts/Controller/FlashLightControll.cs
@@ -12,6 +12,10 @@ public class FlashLightControll : MonoBehaviourPun
     public Light light;
     public float DecraeseBatteryPerUpdate;
     public GameObject TorchLamp;
+    public GameObject FlickerEffect;
+    private bool played = false;
+    private bool playing = false;
+    private int timer = 0;
     
 
     private bool IsOffline;
@@ -38,6 +42,24 @@ public class FlashLightControll : MonoBehaviourPun
     {
         if (LightOn && HasTorchLamp)
         {
+            if (playing)
+            {
+                timer -= 1;
+                if (timer <= 0)
+                {
+                    FlickerEffect.SetActive(false);
+                    SetLightOn(true);
+                }
+            }
+            else if (!played && PlayerManagerLo.instance.CurrentBatteryLevel <= 25 &&
+                     PlayerManagerLo.instance.CurrentBatteryLevel >= 20)
+            {
+                timer = 150;
+                played = true;
+                playing = true;
+                FlickerEffect.SetActive(true);
+            }
+
             PlayerManagerLo.instance.CurrentBatteryLevel -= DecraeseBatteryPerUpdate * Time.deltaTime;
             if (PlayerManagerLo.instance.CurrentBatteryLevel <= 0)
             {
